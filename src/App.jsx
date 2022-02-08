0c34cddd098c3c21d1e511769e2680b7cf1c6975
nabilk11 <khan.nabiln11@gmail.com>
2022-02-07 22:37:47 -0500

    added axios.get function to make api call, .map the results, and then set that to setPokemon state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React, { useState }from 'react';
+import React, { useState, useEffect }from 'react';
 import './App.css';
 import PokemonList from './components/PokemonList';
+import axios from 'axios';
 
 function App() {
   // need to create state for Pokemon
   //pokemon = pokemon data setPokemon = method by which we will update pokemon
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+  const [pokemon, setPokemon] = useState([]);
+
+
+
+// making api call with axios
+axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
+  setPokemon(res.data.results.map(p => p.name))
+})
+
+
   return (
     <div className="App">
       <PokemonList pokemon={pokemon} />
@@ -20,3 +30,8 @@ export default App;
 // step 1 added state with useState, added two default pokemon in just for testing "bulbasaur" "charmander"
 // pokemon is the data that we will need setPokemon is the method to update the state
 // step 2 import PokemonList and added it to the App, also added the prop for pokemon so it can get the pokemon data
+// see pokemon list.jsx for additional steps for rendering list and adding pokemon prop
+// next step installed and imported axios for api call - using it to make api call from pokemon api
+// making api call with axios and using that to setstate
+// then we need to remove the example pokemon from the useState back to an empty array
+// importing useEffect
